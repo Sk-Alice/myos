b00b88a27873197011adf32121277cbb5a2edaa0
守坤 F <3022844681@qq.com>
2022-07-07 14:53:46 +0800

    myos

---
+++ gdt.cpp
@@ -0,0 +1,31 @@
+#include "gdt.h"
+
+GlobalDescriptorTable::GlobalDescriptorTable()
+    : nullSegmentDescriptor(0, 0, 0),
+    unusedSegmentDescriptor(0, 0, 0),
+    codeSegmentDescriptor(0, 64 * 1024 * 1024, 0x9a),
+    dataSegmentDescriptor(0, 64 * 1024 * 1024, 0x92) {
+    uint32_t i[2];
+    i[1] = (uint32_t)this;
+    i[0] = sizeof(GlobalDescriptorTable) << 16;
+    // c++ 使用汇编  asm
+    asm volatile("lgdt (%0)": :"p"((uint8_t*)i) + 2)
+}
+
+GlobalDescriptorTable::~GlobalDescriptorTable() {}
+
+uint16_t GlobalDescriptorTable::DataSegmentDescriptor() {
+    return (uint8_t*)&dataSegmentDescriptor - (uint8_t*)this;
+}
+
+uint16_t GlobalDescriptorTable::CodeSegmentDescriptor() {
+    return (uint8_t*)&codeSegmentDescriptor - (uint8_t*)this;
+}
+
+GlobalDescriptorTable::SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type) {
+    uint8_t* target = (uint8_t*)this;
+
+    if(limit < 65536) {
+        target[6] = 0x80;
+    }
+}
---
+++ gdt.h
@@ -0,0 +1,37 @@
+#ifndef __GDT.H
+#define __GDT.H
+
+#include "types.h"
+
+class GlobalDescriptorTable
+{
+public:
+    class SegmentDescriptor
+    {
+    public:
+        // base 段的起始地址    limit 段的寻址最大范围    type 段的类型(访问权限)
+        SegmentDescriptor(uint32_t base, uint32_t limit, uint8_t type);
+        uint32_t Base();
+        uint32_t Limit();
+    private:
+        uint16_t limit_lo;
+        uint16_t base_lo;
+        uint8_t base_hi;
+        uint8_t type;
+        uint8_t flags_limit_hi;
+        uint8_t base_vhi;
+    } __arrtibute__((packed));      // 告诉编译器取消结构在编译过程中的优化对齐
+
+    SegmentDescriptor nullSegmentDescriptor;
+    SegmentDescriptor unusedSegmentDescriptor;
+    SegmentDescriptor codeSegmentDescriptor;
+    SegmentDescriptor dataSegmentDescriptor;
+
+public:
+    GlobalDescriptorTable();
+    ~GlobalDescriptorTable();
+
+    uint16_t CodeSegmentDescriptor();
+    uint16_t DataSegmentDescriptor();
+};
+#endif
